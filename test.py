76610e7b6f7066da2d44aca12fd96c65168c364f
gender <misha_rol00@mail.ru>
2026-05-12 19:08:26 +0300

    Refactor test.py for improved socket handling

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
 from time import sleep  # noqa: F401
-from random import choice  # noqa: F401
+from random import random
 
 from server.inet import Socket
-from server.request import Request
+from server.request import Request 
+from server.response import Response
 from server.logging import info, warn, error # noqa: F401
 
 
 socket = Socket()
 socket.connect(('localhost', 5000))
-socket.settimeout(None)
 
 req = Request('POST', '/api/admin/login')
-req.header('Connection', 'keep-alive')
-req.json({'username': 'admin', 'password': 'admin123'})
+req.json({
+    'username': 'admin',
+    'password': 'admin123'
+})
 req.to_socket(socket)
 
 res = Request.from_socket(socket)
@@ -24,41 +26,30 @@ if data is None:
 	warn('Входящий запрос неверный')
 	exit()
 token = data['token']
+socket.close()
 
-# states = ['on', 'off']
+socket = Socket()
+socket.connect(('localhost', 5000))
 
-# req = Request('POST', '/api/command/water')
-# req.header('Connection', 'keep-alive')
-# req.json({'state': choice(states), 'token': token})
-# req.to_socket(socket)
-
-# sleep(0.1)
-# req = Request('POST', '/api/command/light')
-# req.header('Connection', 'keep-alive')
-# req.json({'state': choice(states), 'token': token})
-# req.to_socket(socket)
-
-# sleep(0.1)
-# req = Request('POST', '/api/command/fan')
-# req.header('Connection', 'keep-alive')
-# req.json({'state': choice(states), 'token': token})
-# req.to_socket(socket)
-
-# sleep(1)
-# exit()
-
-req = Request('GET', '/api/graph/table')
-req.header('Connection', 'keep-alive')
+req = Request('POST', '/api/ph')
 req.json({
-	'table': 'water',
-	'seconds': 19*60*60, # 1 день
+	'token': token,
+	'level': 0.0
 })
 req.to_socket(socket)
+data = socket.recv(1024)
+print(data)
+socket.close()
 
-res = Request.from_socket(socket)
-if res is None:
-	warn('Входящий запрос неверный')
-	exit()
-print(res.to_text())
+socket = Socket()
+socket.connect(('localhost', 5000))
 
-sleep(1)
+req = Request('GET', '/api/graph/table')
+req.json({
+	'table': 'ph',
+	'seconds': 60
+})
+req.to_socket(socket)
+data = socket.recv(1024**2)
+print(data)
+socket.close()
