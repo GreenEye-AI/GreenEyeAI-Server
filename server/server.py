20283678ad04efe8a901be9f09431db451ef8c87
gender <misha_rol00@mail.ru>
2026-05-12 18:59:41 +0300

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 from typing import Callable
-from socket import socket as Socket, AF_INET, SOCK_STREAM
+from socket import SO_REUSEADDR, SOL_SOCKET, socket as Socket, AF_INET, SOCK_STREAM
 from time import sleep
+import mimetypes
 
 from .logging import info, warn, error
 from .threads import nonblocking
@@ -12,32 +13,7 @@ from .database import DataBase
 from .cluster import Cluster, File
 
 
-CALLBACK_TYPE = Callable[['Server', Request], Response]
-
-
-def get_content_type(path: str) -> str:
-	end = '' 
-	if '.' in path:
-		end = path.rsplit('.', 1)[1]
-	content_type = 'text/plain'
-	match end:
-		case 'css':
-			content_type = 'text/css'
-		case 'html':
-			content_type = 'text/html; charset=utf-8'
-		case 'js':
-			content_type = 'application/javascript'
-		case 'jpg', 'jpeg':
-			content_type = 'image/jpeg'
-		case 'png':
-			content_type = 'image/png'
-		case 'webp':
-			content_type = 'image/webp'
-		case 'svg':
-			content_type = 'image/svg+xml'
-		case 'xml':
-			content_type = 'text/xml'
-	return content_type
+CALLBACK_TYPE = Callable[['Server', Socket, Request], Response | None]
 
 
 class Server:
@@ -49,6 +25,7 @@ class Server:
 	socket: Socket
 	paths: dict[str, dict[str, CALLBACK_TYPE]]
 	
+
 	def __init__(self,
 		data: Data,
 		database: DataBase,
@@ -63,6 +40,7 @@ class Server:
 		self.host = host
 		self.port = port
 
+
 	# используется для добавления обработчика на путь
 	def path(self, method: str, path: str) -> Callable[[CALLBACK_TYPE], None]:
 		def wrapper(func: CALLBACK_TYPE):
@@ -71,74 +49,68 @@ class Server:
 			self.paths[path][method] = func
 		return wrapper
 		
+
 	@nonblocking
 	def accepting(self) -> None:
 		running: bool = True
 		while running:
-			client: Socket
-			ip: str
-			port: int
 			client, (ip, port) = self.socket.accept()
 			self.processing(client, ip, port) # не блокирует поток
+
+
+	def cluster_get_content(self, path: str) -> bytes | None:
+		file = self.cluster[path]
+		if type(file) is File:
+			return file.read()
+
 
 	@nonblocking
 	def processing(self, client: Socket, ip: str, port: int) -> None:
 		info(f'Подключение: {ip}:{port}')
 		client.settimeout(5.0)
-		running: bool = True
-		while running:
+
+		while True:
 			try:
 				req = Request.from_socket(client)
 			except TimeoutError:
-				running = False
-				continue
+				break
 			except ConnectionResetError:
-				running = False
-				continue
+				break
 			except:
 				raise
 
 			if req is None:
-				running = False
 				warn(f'Неверный HTTP: {ip}:{port}')
-				client.send(Response(400).to_bytes())
-				continue
-			
-			if 'Connection' in req.headers:
-				if req.headers['Connection'] == 'keep-alive':
-					client.settimeout(60.0)
-				else:
-					running = False
-			else:
-				running = False
-
-			if req.path not in self.paths:
-				if req.path in self.cluster:
-					file = self.cluster[req.path]
-					if type(file) is File:
-						res = Response(200)
-						res.bytes(file.read())
-						res.header('Content-Type', get_content_type(file.path))
-						res.header('Connection', 'keep-alive' if running else 'close')
-						client.send(res.to_bytes())
-						continue
+				Response(400).to_socket(client)
+				break
 				
-				res = Response(404)
-				res.text("404: Страница не найдена")
-				res.header('Content-Type', 'text/html; charset=utf-8')
-				res.header('Connection', 'keep-alive' if running else 'close')
-				client.send(res.to_bytes())
-				continue
+			if req.path in self.paths:
+				if req.method not in self.paths[req.path]:
+					Response(400).to_socket(client)
+					break
+				res = self.paths[req.path][req.method](self, client, req)
+				if res is not None:
+					res.to_socket(client)
+					if 'Connection' in res.headers:
+						if res.headers['Connection'] == 'keep-alive':
+							continue
 
-			if req.method not in self.paths[req.path]:
-				res = Response(400)
-				res.header('Connection', 'keep-alive' if running else 'close')
-				client.send(res.to_bytes())
-				continue
-	
-			res = self.paths[req.path][req.method](self, req)
-			res.header('Connection', 'keep-alive' if running else 'close')
-			client.send(res.to_bytes())
+			elif (((path := req.path) in self.cluster) or
+				((path := req.path + '.html') in self.cluster)):
+				data = self.cluster_get_content(path)
+				if data is None:
+					Response(404).to_socket(client)
+				else:
+					res = Response(200)
+					mimetype = mimetypes.guess_type(path)[0]
+					if mimetype is not None:
+						res.header('Content-Type', mimetype)
+					res.bytes(data)
+					res.to_socket(client)
+
+			else:
+				Response(404).to_socket(client)
+			break
 
 		info(f'Отключение: {ip}:{port}')
 		client.close()
@@ -147,8 +119,9 @@ class Server:
 	def start(self) -> None:
 		info('Запуск сервера')
 		self.socket: Socket = Socket(AF_INET, SOCK_STREAM)
+		self.socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 		self.socket.bind((self.host, self.port))
-		self.socket.listen()
+		self.socket.listen(128)
 		self.accepting() # не блокирует поток
 
 		try:
