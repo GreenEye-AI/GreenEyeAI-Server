a75737270e2f7e55db1b9430332322c53f63331f
gender <misha_rol00@mail.ru>
2026-05-12 19:06:47 +0300

    Update esp_paths.py

--- esp_paths.py
+++ esp_paths.py
@@ -1,24 +1,25 @@
 from time import time
+from socket import socket as Socket
 
 from server import Server, Request, Response
 from server.logging import info, warn, error  # noqa: F401
 
 
-def esp_sens_path(server: Server, req: Request) -> Response:
+def esp_sens_path(server: Server, client: Socket, req: Request) -> Response | None:
 	data: dict = req.get_json()
 	if server.database is None:
 		return Response(500)
-	if data and 'temperature' in data and 'humidity' in data:
+	if data and ('temperature' in data) and ('humidity' in data):
 		server.database.execute(
 			'INSERT INTO sensors (timestamp, temperature, humidity) VALUES (?, ?, ?)',
 			(time(), data['temperature'], data['humidity'])
 		)
 	else:
 		return Response(400).text('Не полные данные')
-	return Response(200)
+	return Response(200).header('Connection', 'keep-alive')
 
 
-def esp_gcmd_path(server: Server, req: Request) -> Response:
+def esp_gcmd_path(server: Server, client: Socket, req: Request) -> Response | None:
 	commands: list[tuple[int, str, str]] = server.data.commands
 	command_id, device, action = (0, 'None', 'off')
 	with server.data.commands_lock:
@@ -29,10 +30,10 @@ def esp_gcmd_path(server: Server, req: Request) -> Response:
 		"id": command_id,
 		"device": device,
 		"action": action
-	})
+	}).header('Connection', 'keep-alive')
 
 
-def esp_dcmd_path(server: Server, req: Request) -> Response:
+def esp_dcmd_path(server: Server, client: Socket, req: Request) -> Response | None:
 	commands: list[tuple[int, str, str]] = server.data.commands
 	data = req.get_json()
 	if data is None:
@@ -49,5 +50,5 @@ def esp_dcmd_path(server: Server, req: Request) -> Response:
 		f'INSERT INTO {device} (timestamp, state) VALUES (?, ?)',
 		(time(), True if action == 'on' else False)
 	)
-	info("Команда удалена")
-	return Response(200)
+	# info("Команда удалена")
+	return Response(200).header('Connection', 'keep-alive')
