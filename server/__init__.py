3d8b3eff6820ca4e080bc734d9075de6a04966f0
ssvvad <ssvvaadd0@gmail.com>
2026-05-18 21:46:29 +0300

    Delete server directory

--- server/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = ['Server', 'Request', 'Response', 'Data', 'DataBase']
-
-from .request import Request
-from .response import Response
-from .server import Server
-from .data import Data
-from .database import DataBase
